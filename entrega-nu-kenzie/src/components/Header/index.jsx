6400482c4247d61e28c6dcfab4cba4172890bf75
Pedro26fer <p2698fernandes@gmail.com>
2023-03-08 14:17:24 -0300

    feat:introduzindo styled-components

--- entrega-nu-kenzie/src/components/Header/index.jsx
+++ entrega-nu-kenzie/src/components/Header/index.jsx
@@ -2,6 +2,7 @@ import { HeaderStyled } from "./style"
 
 export const Header = () => {
     return (
+        
         <HeaderStyled>
             <h1>Nu Kenzie</h1>
             <button>Inicio</button>

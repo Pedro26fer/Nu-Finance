67b71996d89dbe35c58d3c82a5f7fdbeca28a6c2
Pedro26fer <p2698fernandes@gmail.com>
2023-03-29 23:12:15 -0300

    feat:integration with PostgreSQL

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,17 +1,27 @@
 import { DataSource } from "typeorm";
-require('dotenv').config()
+require("dotenv").config();
+import { User } from "./entities/user.entity";
+import { Accounts } from "./entities/accounts.entity";
+import { Transactions } from "./entities/transactions.entity";
+import {createTables1680141828119} from "./migrations/1680141828119-createTables"
 
 
-export const AppDataSource = new DataSource({
-    type: 'postgres',
-    host: 'localhost',
-    username: process.env.POSTGREE_USER,
-    password: process.env.POSTGREE_PASSWORD,
-    database: process.env.POSTGREE_DATABASE,
 
-    synchronize: false,
-    logging: true,
 
-    entities:['src/entities/*.ts'],
-    migrations: ['src/migrations/*.ts']
-})
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",    
+  username: process.env.POSTGREE_USER,
+  password: process.env.POSTGREE_PASSWORD,
+  database: process.env.POSTGREE_DATABASE,
+
+  synchronize: false,
+  logging: true,
+
+  entities: [User, Accounts, Transactions],
+  migrations: [createTables1680141828119]
+  
+});
+
+
+export default AppDataSource;

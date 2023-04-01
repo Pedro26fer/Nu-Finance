dda869a2610f663a5c16c6f252e5e5118a9b6464
Pedro26fer <p2698fernandes@gmail.com>
2023-04-01 00:32:49 -0300

    fix: resolving the crash in server if registering email is already being used

--- server/src/routes/sigupUser.routes.ts
+++ server/src/routes/sigupUser.routes.ts
@@ -1,8 +1,7 @@
-import { emailAlreadyExistsMiddleware } from "../middlewares/emailAlreadyExixts.middleware";
 import signUpUserController from "../controllers/user/signupUser.controller";
 import { Express } from "express";
 
 
 export const signUpRoute = (app: Express) => {
-    app.post("/signup", emailAlreadyExistsMiddleware, signUpUserController)
+    app.post("/signup", signUpUserController)
 }
--- server/src/services/user/signupUser.service.ts
+++ server/src/services/user/signupUser.service.ts
@@ -2,11 +2,18 @@ import AppDataSource from "../../data-source";
 import { User } from "../../entities/user.entity";
 import bcrypt from 'bcrypt';
 import { IUserSignUp} from "../../interfaces/user";
+import { AppError } from "../../error/appError";
 
 
 const signUpUserService = async ({name,email,password} : IUserSignUp) => {
 
     const userRepository = AppDataSource.getRepository(User)
+
+    const emailAlreadyExists = await userRepository.findOne({where: {email}})
+
+    if(emailAlreadyExists){
+        throw new AppError(403,"Email already registered")
+    }
     
     const newUser = await userRepository.save({
         name,
